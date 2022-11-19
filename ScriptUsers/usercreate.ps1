a7d7911cd2313daa65debb6b1ddb461c8944050d
Charly0n3 <109216376+Charly0n3@users.noreply.github.com>
2022-11-19 10:53:49 +0100

    Add files via upload

---
+++ ScriptUsers/usercreate.ps1
@@ -0,0 +1,21 @@
+﻿Write-Host `CREACIÓN DE USUARIOS CON CSV`
+
+Import-Csv -Delimiter ";" -Path ".\usuarios.csv" | ForEach-Object {
+
+<# Cogemos como variables la primera linea de cada columna que serán los datos de los usuarios #>
+
+
+$GivenName=$_.GivenName
+$Surname=$_.Surname
+$Name=$_.Name
+$DisplayName=$_.DisplayName
+$Password=$_.Password
+$Upn=$User+'@'+'intruders.local'
+$User=$_.SamAccountName
+$enc_pasword=(ConvertTo-SecureString -AsPlainText $Password -Force)
+
+New-ADUser -Name $Name -GivenName $GivenName -Surname $Surname -SamAccountName $User -UserPrincipalName $Upn -Enable $true -AccountPassword $enc_pasword -Path "OU=testers,DC=intruders,DC=local"
+
+
+}
+
